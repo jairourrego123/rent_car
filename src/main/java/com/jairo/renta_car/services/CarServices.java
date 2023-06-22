b42850ff7477e1c96120230fb1d8094f57fc2d75
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-06-22 16:49:41 -0500

    Services Car V1

--- src/main/java/com/jairo/renta_car/services/CarServices.java
+++ src/main/java/com/jairo/renta_car/services/CarServices.java
@@ -4,7 +4,9 @@ import com.jairo.renta_car.models.Car;
 import com.jairo.renta_car.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.swing.*;
 import java.util.List;
+import java.util.Optional;
 
 public class CarServices {
 
@@ -14,7 +16,27 @@ public class CarServices {
     public List<Car> getAllCar(){
 
     }
-    public List<Car> getAllCarByYear(){
+    public List<Car> getAllCarByYear(Integer year){
         
     }
+
+   public Optional<Car> GetById(Integer id){
+
+   }
+
+   public Optional<Car> getCarByPlate(Spring plate){
+
+   }
+
+    public Car insertCar(Car car){
+
+    }
+
+    public Car updateCar(Car car){
+
+    }
+    public boolean deleteCar(Integer carId){
+
+    }
+
 }
