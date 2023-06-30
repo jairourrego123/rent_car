7e382374101745d08a887fc498591d63ba7dce1c
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-06-30 17:34:03 -0500

    Loggin GitHub

---
+++ src/main/java/com/jairo/renta_car/security/SecurityAdapter.java
@@ -0,0 +1,18 @@
+package com.jairo.renta_car.security;
+
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class SecurityAdapter extends WebSecurityCongurerAdapter {
+ //   @Override
+  //  protected void configure(HttpSecurity http) throws  Exception{
+  //      http.authorizeRequests(
+  //              a -> a.antMatchers(
+  //                      "/"
+  //              )
+  //      )
+  //  }
+// }
