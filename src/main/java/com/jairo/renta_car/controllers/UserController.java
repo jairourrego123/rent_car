d2916a9247911913c5fb9e93e745de544b453243
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-07-07 21:15:43 -0500

    Security Pages

--- src/main/java/com/jairo/renta_car/controllers/UserController.java
+++ src/main/java/com/jairo/renta_car/controllers/UserController.java
@@ -5,15 +5,15 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
+
 import java.util.Collections;
 import java.util.Map;
-import java.util.Objects;
+
 
 @RestController
 public class UserController {
     @GetMapping("/user")
-    public Map<String, Objects> user(@AuthenticationPrincipal OAuth2User userInfo){ //un mapa (estructura clave valor) de Java , nos autenticamos y nos va a retornar un objeto Auth2User y lo vamos a llamar user Info
+    public Map<String,Object> user(@AuthenticationPrincipal OAuth2User userInfo){ //un mapa (estructura clave valor) de Java , nos autenticamos y nos va a retornar un objeto Auth2User y lo vamos a llamar user Info
        System.out.println(userInfo);
        System.out.println(userInfo.getAttributes());
         return Collections.singletonMap("name",userInfo.getAttribute("name")); // Arma un mapa con la clave name y el valor es del objeto userinfo traemos el atributo login
--- src/main/java/com/jairo/renta_car/security/SecurityAdapter.java
+++ src/main/java/com/jairo/renta_car/security/SecurityAdapter.java
@@ -1,12 +1,27 @@
 package com.jairo.renta_car.security;
 
-
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**",
+                                "/Reservations/**","/clients/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+        http.cors().and().csrf().disable();
+        // @formatter:on
+    }
+}
